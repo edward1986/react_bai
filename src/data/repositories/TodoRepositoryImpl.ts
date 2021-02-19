a2a132b704f17714d1d43ed5eccb516a86dffab0
Lorilla Family <edwardlorilla1986@gmail.com>
2021-02-19 23:45:11 +0800

    add test todo repo commit

--- src/data/repositories/TodoRepositoryImpl.ts
+++ src/data/repositories/TodoRepositoryImpl.ts
@@ -7,14 +7,22 @@ class TodoDTO {
     isCompleted = false
 }
 
+let todos: any
+// eslint-disable-next-line prefer-const
+todos = [
+    {
+        id: 0,
+        task: "",
+        isCompleted: false,
+    },
+]
+
 export class TodoRepositoryImpl implements TodoRepository {
     jsonUrl =
         "https://gist.githubusercontent.com/janithl/6bfbd787a0361c170ac760e8fb5ba0fd/raw/a0ffacb7c0fc21a0266371f632cf4107f80362f4/todolist.json"
 
     async GetTodos(): Promise<Todo[]> {
-        const res = await fetch(this.jsonUrl)
-        const jsonData = await res.json()
-        return jsonData.map((todo: TodoDTO) => new Todo(todo.id, todo.task, todo.isCompleted))
+        return todos.map((todo: TodoDTO) => new Todo(todo.id, todo.task, todo.isCompleted))
     }
 
     PostTodo(todo: Todo): Todo {
